1f8f7ec574622fad86117dd7f19471d2c93ac2f6
emortal <nathanpenwill88@gmail.com>
2022-07-16 00:06:05 +0100

    a

--- src/main/java/dev/emortal/datadependency/MinestomExtension.java
+++ src/main/java/dev/emortal/datadependency/MinestomExtension.java
@@ -1,10 +1,12 @@
 package dev.emortal.datadependency;
 
 import net.minestom.server.extensions.Extension;
+import org.litote.kmongo.serialization.SerializationClassMappingTypeService;
 
 public class MinestomExtension extends Extension {
     @Override
     public void initialize() {
+        System.setProperty("org.litote.mongo.mapping.service", SerializationClassMappingTypeService.class.getName());
         getLogger().info("[DataDependency] Initialized!");
     }
 
--- src/main/java/dev/emortal/datadependency/VelocityPlugin.java
+++ src/main/java/dev/emortal/datadependency/VelocityPlugin.java
@@ -1,7 +1,17 @@
 package dev.emortal.datadependency;
 
+import com.google.inject.Inject;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
+import org.litote.kmongo.serialization.SerializationClassMappingTypeService;
+
+import java.util.logging.Logger;
 
 @Plugin(id = "datadependency", name = "DataDependency")
 public class VelocityPlugin {
+    @Inject
+    public void VelocityTest(ProxyServer server, Logger logger) {
+        System.setProperty("org.litote.mongo.mapping.service", SerializationClassMappingTypeService.class.getName());
+        logger.info("[DataDependency] Initialized!");
+    }
 }
